b1a9c8ee80d4f1fb90e736abc14fe61af6f75a81
k.lantsov <k.lantsov@yahoo.com>
2022-06-28 13:41:08 +0300

    Test Instance Lifecycle

--- src/test/java/com/company/MethodsOrderedByNameTest.java
+++ src/test/java/com/company/MethodsOrderedByNameTest.java
@@ -1,32 +1,27 @@
 package com.company;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class MethodsOrderedByOrderIndex {
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodsOrderedByNameTest {
 
-    @Order(1)
     @Test
     void testD() {
         System.out.println("Running test D");
     }
 
-    @Order(2)
     @Test
     void testA() {
         System.out.println("Running test A");
     }
 
-    @Order(3)
     @Test
     void testC() {
         System.out.println("Running test C");
     }
 
-    @Order(4)
     @Test
     void testB() {
         System.out.println("Running test B");
